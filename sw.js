d1e47cb49478e7d16c9b3908dcd69555f3edd8fe
1000net2000-oss <1000net2000@gmail.com>
2026-04-24 06:29:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'otchet-v12';
+const CACHE = 'otchet-v13';
 const FILES = ['./index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
