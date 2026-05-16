cf5e8edad4c35c45b70f5d5d41a921600ca72bbd
1000net2000-oss <1000net2000@gmail.com>
2026-05-16 07:57:15 +0200

    Update app.js
    Remove ellabelprok

--- app.js
+++ app.js
@@ -275,7 +275,6 @@ function render() {
   if (elLabelSklad) elLabelSklad.textContent = lang === 'pl' ? 'Oddano dystr.' : 'Отдал дистр.';
   const elLabelMbSklad = document.getElementById('labelMbSklad');
   if (elLabelMbSklad) elLabelMbSklad.textContent = lang === 'pl' ? 'Oddano MB' : 'Отдал МБ';
-  const elLabelProk = document.getElementById('labelProk');
   document.getElementById('labelWarehouse').textContent  = t('warehouse');
   document.getElementById('labelHighway').textContent    = t('highway');
   document.getElementById('labelWarehouseDays').textContent = t('days');
